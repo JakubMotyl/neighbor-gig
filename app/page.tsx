4e2ebbacf0060de8b2a884e081ab4d51bc492b4d
Jakub Motyl <kubamotyl16@gmail.com>
2026-05-19 19:36:03 +0200

    feat: complete PopularTasks section with custom URL Query parameters for filter handling via '?category='

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/Home/Hero";
+import PopularTasks from "@/components/Home/PopularTasks";
 
 export default function Home() {
     return (
         <header className="min-h-dvh">
             <Navbar />
             <Hero />
+            <PopularTasks />
         </header>
     );
 }
