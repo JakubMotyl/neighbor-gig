d9241f23b5661f29b302770fa1db3256084e9c82
Jakub Motyl <kubamotyl16@gmail.com>
2026-07-30 20:50:34 +0200

    feat: setup auth.js config and basic session managment

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,13 @@ import TrustAndPro from "@/components/Home/TrustAndPro";
 import HowItWorks from "@/components/Home/HowItWorks";
 import ScrollHandler from "@/components/Home/ScrollHandler";
 import FaqSection from "@/components/Home/FaqSection";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
+export default async function HomePage() {
+    const session = await auth();
+    if (!session) redirect("/rejestracja");
+
     return (
         <>
             <ScrollHandler />
