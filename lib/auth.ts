7bf6b1b2f9e54cb7e77d96e3536418122b4da241
Jakub Motyl <kubamotyl16@gmail.com>
2026-08-01 12:49:57 +0200

    feat: add Auth.js Prisma adapter schema with password support

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,33 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
+import { prisma } from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-    providers: [Google],
+const adapter = PrismaAdapter(prisma);
+
+export const { auth, handlers, signIn } = NextAuth({
+    adapter,
+    providers: [
+        Google,
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            authorize: async (credentials) => {
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials.email,
+                        password: credentials.password,
+                    },
+                });
+
+                if (!user) {
+                    throw new Error("Invalid credentials.");
+                }
+                return user;
+            },
+        }),
+    ],
 });
